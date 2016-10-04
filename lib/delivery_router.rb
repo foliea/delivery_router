cbfd1e6d4d1ff02e6763b88d0956bcf942d97084
Adrien Folie <folie.adrien@gmail.com>
2016-10-04 14:49:07 +0200

    add basic skeleton for delivery router

--- lib/delivery_router.rb
+++ lib/delivery_router.rb
@@ -1,1 +1,29 @@
+Customer = Struct.new(:id, :x, :y)
 
+Restaurant = Struct.new(:id, :cooking_time, :x, :y)
+
+Rider = Struct.new(:id, :speed, :x, :y)
+
+class DeliveryRouter
+  def initialize(restaurants, customers, riders)
+    @restaurants = restaurants
+    @customers   = customers
+    @riders      = riders
+  end
+
+  def add_order(customer:, restaurant:)
+
+  end
+
+  def clear_orders(customer:)
+
+  end
+
+  def route(rider:)
+    []
+  end
+
+  def delivery_time(customer:)
+    60
+  end
+end
