9ce551ff6d36c24c26b2a141e2be0d5f047c1eab
Adrien Folie <folie.adrien@gmail.com>
2016-10-06 04:23:06 +0200

    add test case when two riders are the same block and two customers orders from the same restaurant

--- spec/delivery_router_spec.rb
+++ spec/delivery_router_spec.rb
@@ -39,12 +39,15 @@ describe DeliveryRouter do
           Customer.new(:id => 4, :x => 0, :y => 5),
           Customer.new(:id => 5, :x => 0, :y => 7),
           Customer.new(:id => 6, :x => 0, :y => 7),
+          Customer.new(:id => 7, :x => 5, :y => 13),
+          Customer.new(:id => 8, :x => 6, :y => 13),
         ]
       @restaurants = [
         Restaurant.new(:id => 3, :cooking_time => 15, :x => 0, :y => 0),
         Restaurant.new(:id => 4, :cooking_time => 35, :x => 5, :y => 5),
         Restaurant.new(:id => 5, :cooking_time => 5,  :x => 0, :y => 4),
         Restaurant.new(:id => 6, :cooking_time => 5,  :x => 0, :y => 8),
+        Restaurant.new(:id => 7, :cooking_time => 10, :x => 6, :y => 14),
       ]
       @riders = [
         Rider.new(:id => 1, :speed => 10, :x => 2, :y => 0),
@@ -53,6 +56,8 @@ describe DeliveryRouter do
         Rider.new(:id => 4, :speed => 5,  :x => 1, :y => 4),
         Rider.new(:id => 5, :speed => 10, :x => 1, :y => 12),
         Rider.new(:id => 6, :speed => 10, :x => 0, :y => 11),
+        Rider.new(:id => 7, :speed => 10, :x => 4, :y => 14),
+        Rider.new(:id => 8, :speed => 10, :x => 4, :y => 14),
       ]
       @delivery_router = DeliveryRouter.new(@restaurants, @customers, @riders)
     end
@@ -177,6 +182,40 @@ describe DeliveryRouter do
 
         it "delights customer 6" do
           expect(@delivery_router.delivery_time(:customer => 6)).to be < 60
+        end
+      end
+    end
+
+    context "given customer 7 orders from restaurant 7" do
+      before(:all) do
+        @delivery_router.add_order(:customer => 7, :restaurant => 7)
+      end
+
+      it "sends rider 7 to customer 7 through restaurant 7" do
+        route = @delivery_router.route(:rider => 7)
+        expect(route.length).to eql(2)
+        expect(route[0].id).to eql(7)
+        expect(route[1].id).to eql(7)
+      end
+
+      it "delights customer 7" do
+        expect(@delivery_router.delivery_time(:customer => 7)).to be < 60
+      end
+
+      context "given customer 8 orders from restaurant 7" do
+        before(:all) do
+          @delivery_router.add_order(:customer => 8, :restaurant => 7)
+        end
+
+        it "sends rider 8 to customer 8 through restaurant 7" do
+          route = @delivery_router.route(:rider => 8)
+          expect(route.length).to eql(2)
+          expect(route[0].id).to eql(7)
+          expect(route[1].id).to eql(8)
+        end
+
+        it "delights customer 8" do
+          expect(@delivery_router.delivery_time(:customer => 8)).to be < 60
         end
       end
     end
